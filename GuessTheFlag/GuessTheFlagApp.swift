95f05388f76e4187e32c855bbd60cd8055d7cc3d
Ojidaemo <vit.martsinovich@gmail.com>
2023-07-14 12:45:46 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Vitali Martsinovich on 2023-07-14.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
